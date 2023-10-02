2ffed206058241be8838c55cd19b92568df06d02
Bram Lahousse <70818886+LahousseBram@users.noreply.github.com>
2023-10-02 20:16:21 +0200

    rewrote code

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request, session
+from flask import Flask, request
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # You should use a secure secret key in a production environment
 
-if 'high_score' not in session:
-    session['high_score'] = 0
-if 'total_bets' not in session:
-    session['total_bets'] = 0
+# Initialize global variables
+high_score = 0
+total_bets = 0
 
 @app.route("/add-statistic", methods=['POST'])
 def add_statistic():
@@ -14,10 +12,12 @@ def add_statistic():
     hs = int(request.form.get("high_score"))
     cb = request.form.get("current_bets")
 
-    session['total_bets'] += 1
+    global high_score, total_bets
     
-    if hs > session['high_score']:
-        session['high_score'] = hs
+    total_bets += 1
+    
+    if hs > high_score:
+        high_score = hs
 
     log_info(cs, hs, cb)
 
@@ -27,8 +27,8 @@ def log_info(cs, hs, cb):
     print("--------------------------")
     print(f"Bot made new bet")
     print(f"Its streak: {cs}")
-    print(f"Current high score: {session['high_score']}")
-    print(f"Total bets: {session['total_bets']}")
+    print(f"Current high score: {high_score}")
+    print(f"Total bets: {total_bets}")
     print(f"Current Amount of Bets for this Bot: {cb}")
     print("--------------------------")
 
